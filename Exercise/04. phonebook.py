20e7d60f555ddb528a2414948b659c80d320e387
me_madness <p_rangel@abv.bg>
2023-11-03 19:17:18 +0100

    04 done by the lecture

--- Exercise/04. phonebook.py
+++ Exercise/04. phonebook.py
@@ -1,5 +1,20 @@
 # First task from the lecture
 
+phone_book = {}
+while True:
+    entry = input()
+    if "-" not in entry:
+        break
+    name, phone_number = entry.split("-")
+    phone_book[name] = phone_number
+searches = int(entry)
+for search in range(searches):
+    searched_name = input()
+    if searched_name in phone_book.keys():
+        print(f"{searched_name} -> {phone_book[searched_name]}")
+    else:
+        print(f"Contact {searched_name} does not exist")    
+
 
 
 # Second task from me me
