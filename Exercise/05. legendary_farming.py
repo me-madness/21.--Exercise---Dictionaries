3ef544f5681e8e11efde648f01ae9e6f51eea79c
me_madness <p_rangel@abv.bg>
2023-11-03 19:37:31 +0100

    05 Done by the lecture

--- Exercise/05. legendary_farming.py
+++ Exercise/05. legendary_farming.py
@@ -1,6 +1,33 @@
 # First task from the lecture
 
-
-
+items = {"shards": 0, "fragments": 0, "motes": 0}
+current_items = input().split()
+obtained = False
+while not obtained:
+    for index in range(0, len(current_items), 2):
+        value = int(current_items[index])
+        key = current_items[index + 1].lower()
+        if key not in items.keys():
+            items[key] = 0
+        items[key] += value
+        if items["shards"] >= 250:
+            print(f"Shadowmourne obtained!")
+            items["shards"] -= 250 
+            obtained = True 
+        elif items["fragments"] >= 250:
+            print(f"Shadowmourne obtained!") 
+            items["fragments"] -= 250 
+            obtained = True   
+        elif items["motes"] >= 250:
+            print(f"Shadowmourne obtained!")
+            items["motes"] -= 250  
+            obtained = True 
+        if obtained:
+            break             
+    current_items = input().split()  
+for  material, quantity in items.items():  
+    print(f"{material}: {quantity}")    
+    
+      
 # Second task from me me
 
