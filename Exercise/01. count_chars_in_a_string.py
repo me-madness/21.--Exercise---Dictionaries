35f2083ff096651544873752476b4d11fe075416
me_madness <p_rangel@abv.bg>
2023-11-03 18:43:47 +0100

    start with second task

--- Exercise/01. count_chars_in_a_string.py
+++ Exercise/01. count_chars_in_a_string.py
@@ -3,11 +3,11 @@
 symbols = [character for character in input() if character != " "]
 letters = {}
 
-for symbol in symbols:
-    if symbol not in letters.keys():  # by default is if symbol not in letters
-        letters[symbol] = 0
-    letters[symbol] += 1
-    
+for symbol in symbols:                                                          # Second way
+    if symbol not in letters.keys():  # by default is if symbol not in letters  # if symbol not in letters.keys():
+        letters[symbol] = 0                                                     #   letters[symbol] = 1
+    letters[symbol] += 1                                                        # else: 
+                                                                                #   letters[symbol] += 1
 for symbol, occurrences in letters.items():
     print(f"{symbol} -> {occurrences}")        
 
--- Exercise/02. a_miner_task.py
+++ Exercise/02. a_miner_task.py
@@ -1,5 +1,17 @@
 # First task from the lecture
 
+resources = {}
+
+while True:
+    current_resources = input()
+    if current_resources == "stop":
+        break
+    current_quantity = int(input())
+    if current_resources not in resources.keys():
+        resources[current_resources] = 0
+    resources[current_resources] += current_quantity
+        
+
 
 
 # Second task from me me
