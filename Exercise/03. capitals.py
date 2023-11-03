ed7ac42095e61b634f69ff00e5aa8ab58367c814
me_madness <p_rangel@abv.bg>
2023-11-03 19:05:34 +0100

    03 Done by the lecture

--- Exercise/03. capitals.py
+++ Exercise/03. capitals.py
@@ -1,8 +1,16 @@
 # First task from the lecture
 
+#First way
 countries = input().split()(", ")
 capitals = input().split()(", ")
 final_information = {countries[index]: capitals[index] for index in range(len(capitals))}
+for country, capital in countries.items():
+    print(f"{country} -> {capital}")
+    
+#Second way
+countries = input().split()(", ")
+capitals = input().split()(", ")
+final_information = dict(zip(countries, capitals))
 for country, capital in countries.items():
     print(f"{country} -> {capital}")
 
