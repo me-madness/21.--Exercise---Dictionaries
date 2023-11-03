ba84b04db9e7484dcaae6ab9a81e38fd42a3928b
me_madness <p_rangel@abv.bg>
2023-11-03 18:58:44 +0100

    03. Done by the lecture

--- Exercise/03. capitals.py
+++ Exercise/03. capitals.py
@@ -1,8 +1,10 @@
 # First task from the lecture
 
-
-
-
+countries = input().split()(", ")
+capitals = input().split()(", ")
+final_information = {countries[index]: capitals[index] for index in range(len(capitals))}
+for country, capital in countries.items():
+    print(f"{country} -> {capital}")
 
 
 # Second task from me me
