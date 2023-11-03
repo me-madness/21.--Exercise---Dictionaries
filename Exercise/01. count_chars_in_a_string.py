b405ec2278047d0bfc8b21db263fe6dc99375957
me_madness <p_rangel@abv.bg>
2023-11-03 18:17:37 +0100

    01 First task finish by the lecture

--- Exercise/01. count_chars_in_a_string.py
+++ Exercise/01. count_chars_in_a_string.py
@@ -1,10 +1,15 @@
 # First task from the lecture
 
 symbols = [character for character in input() if character != " "]
+letters = {}
 
-
-
-
+for symbol in symbols:
+    if symbol not in letters.keys():  # by default is if symbol not in letters
+        letters[symbol] = 0
+    letters[symbol] += 1
+    
+for symbol, occurrences in letters.items():
+    print(f"{symbol} -> {occurrences}")        
 
 # Second task from me me
 
