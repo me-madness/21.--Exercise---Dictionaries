f09ac040c58b197c4dbc1ad0f9f54dcd30ea8098
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-05 22:17:46 +0100

    06. Begining of my solution

--- Exercise/06. orders.py
+++ Exercise/06. orders.py
@@ -1,6 +1,14 @@
 # First task from the lecture
 
+products = input()
 
+while True:
+    if products == "buy":
+        break
+    products_data = input().split(" ")
+    name = products_data[0]
+    price = products_data[0]
+    quantity = products_data[0]
 
 # Second task from me me
 
